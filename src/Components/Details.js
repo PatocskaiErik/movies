65fb76fd9a829144194a60d1dd17652a4d16a03d
Erik <patoerik93@gmail.com>
2022-02-26 18:34:12 +0100

    Deleted video

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,6 +1,61 @@
-import backgroundVideo from "../videos/Top10.mp4";
+import { request } from "graphql-request";
+import { useEffect, useState } from "react";
 
-const Details = (movie) => {
-  return <div className="commercial-container">DETAILS</div>;
+const Details = ({ title }) => {
+  const [movies, setMovies] = useState();
+
+  const searchMovies = async () => {
+    console.log("submitting");
+
+    const query = `{
+    searchMovies(query: "${title}") {
+    id
+    name
+    overview
+    releaseDate
+    cast {
+      id
+      person {
+        name
+      }
+      role {
+        ... on Cast {
+          character
+        }
+      }
+    }
+  }
+}`;
+
+    const url = "https://tmdb.sandbox.zoosh.ie/dev/graphql";
+
+    try {
+      request("https://tmdb.sandbox.zoosh.ie/dev/graphql", query).then(
+        (data) => {
+          const response = data.searchMovies;
+          setMovies(response);
+          console.log(response);
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  if (!movies) {
+    return <div>Betöltés</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => {
+        return <div>{movie.name}</div>;
+      })}
+    </div>
+  );
 };
 export default Details;
