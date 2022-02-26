a5bbd78b11fd5b87d28d3fab7613bab3932d1843
Erik <patoerik93@gmail.com>
2022-02-26 18:33:55 +0100

    Added state into app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Home from "./Components/Home";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Components/Details";
+import { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
+
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
+            <Route index element={<Home title={title} setTitle={setTitle} />} />
           </Route>
           <Route path="/details">
-            <Route index element={<Details />} />
+            <Route index element={<Details title={title} />} />
           </Route>
         </Routes>
       </BrowserRouter>
