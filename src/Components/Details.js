f750f13a665dd3f7e4ad1a04936e5422edf2bb95
Erik <patoerik93@gmail.com>
2022-02-28 18:28:48 +0100

    Imported model and got a movieName ref

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Popcorn from "../picture/popcorn.jpeg";
 import { MetroSpinner } from "react-spinners-kit";
+import ContentModal from "./ContentModal";
 
 import "../App.css";
 
@@ -12,8 +13,6 @@ const Details = ({ title }) => {
   let moment = require("moment");
 
   const searchMovies = async () => {
-    console.log("submitting");
-
     const query = `{
     searchMovies(query: "${title}") {
     id
@@ -50,7 +49,6 @@ const Details = ({ title }) => {
         (data) => {
           const response = data.searchMovies;
           setMovies(response);
-          console.log(response);
         }
       );
     } catch (err) {
@@ -95,45 +93,47 @@ const Details = ({ title }) => {
       <div className="border">
         {movies.map((movie) => {
           return (
-            <figure className="movie" key={movie.id}>
-              <div className="movie__hero">
-                <img
-                  src={movie.poster == null ? Popcorn : movie.poster.large}
-                  alt="Poster"
-                  className="movie__img"
-                />
-              </div>
-              <div className="movie__content">
-                <div className="movie__title">
-                  <h1 className="heading__primary">
-                    {movie.name} <i className="fas fa-fire"></i>
-                  </h1>
-                  <div className="movie__tag movie__tag--1">
-                    #{movie.genres[0] ? movie.genres[0].name : ""}
+            <ContentModal key={movie.id} movieName={movie.name}>
+              <figure className="movie" key={movie.id}>
+                <div className="movie__hero">
+                  <img
+                    src={movie.poster == null ? Popcorn : movie.poster.large}
+                    alt="Poster"
+                    className="movie__img"
+                  />
+                </div>
+                <div className="movie__content">
+                  <div className="movie__title">
+                    <h1 className="heading__primary">
+                      {movie.name} <i className="fas fa-fire"></i>
+                    </h1>
+                    <div className="movie__tag movie__tag--1">
+                      #{movie.genres[0] ? movie.genres[0].name : ""}
+                    </div>
+                    <div className="movie__tag movie__tag--2">
+                      #{movie.genres[1] ? movie.genres[1].name : ""}
+                    </div>
                   </div>
-                  <div className="movie__tag movie__tag--2">
-                    #{movie.genres[1] ? movie.genres[1].name : ""}
+                  <p className="movie__description">{movie.overview}</p>
+                  <div className="movie__details">
+                    <p className="movie__detail">
+                      <span className="icons icons-grey">
+                        <i className="fas fa-clock"></i>{" "}
+                      </span>
+                      Duration: {Math.floor(movie.runtime / 60)}h{" "}
+                      {movie.runtime % 60} m
+                    </p>
+                    <p className="movie__detail">
+                      Release date:{" "}
+                      {moment.utc(movie.releaseDate).format("MM/DD/YYYY")}
+                    </p>
+                    <p className="scores">
+                      Scores: {movie.score ? movie.score : ""}
+                    </p>
                   </div>
                 </div>
-                <p className="movie__description">{movie.overview}</p>
-                <div className="movie__details">
-                  <p className="movie__detail">
-                    <span className="icons icons-grey">
-                      <i className="fas fa-clock"></i>{" "}
-                    </span>
-                    Duration: {Math.floor(movie.runtime / 60)}h{" "}
-                    {movie.runtime % 60} m
-                  </p>
-                  <p className="movie__detail">
-                    Release date:{" "}
-                    {moment.utc(movie.releaseDate).format("MM/DD/YYYY")}
-                  </p>
-                  <p className="scores">
-                    Scores: {movie.score ? movie.score : ""}
-                  </p>
-                </div>
-              </div>
-            </figure>
+              </figure>
+            </ContentModal>
           );
         })}
       </div>
