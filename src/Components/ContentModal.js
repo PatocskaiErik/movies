5a54fd694402f45f77f74672730fd8dd2dc8d3e1
Erik <patoerik93@gmail.com>
2022-03-07 10:52:47 +0100

    Added max height for no picture

--- src/Components/ContentModal.js
+++ src/Components/ContentModal.js
@@ -110,6 +110,7 @@ const ContentModal = ({ children, movieID, moviePoster }) => {
                               actor.profile_path
                             : "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
                         }
+                        style={{ height: "278px" }}
                       />
                       <div className="actor-name">{actor.name}</div>
                     </div>
